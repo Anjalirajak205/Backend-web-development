de706cbbcad933345a3212eb38d2274e2b419140
Anjalirajak205 <anjalirajak205@gmail.com>
2025-11-27 14:56:22 +0530

    27 november

--- InstaClone/Server/Upload.js
+++ InstaClone/Server/Upload.js
@@ -1,19 +1,27 @@
-let mongoose = require('mongoose')
-
-let uploadSchema= mongoose.Schema({
-     name: { 
-        type: String, required: true 
-    },
+let mongoose= require('mongoose')
+let uploadSchema=   mongoose.Schema({
     imgUrl:{
         type:String,
-        required:true
+        // require:true
     },
     likeCount:{
         type:Number,
         default:0
     }
+    ,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    likedBy: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      }]
+
+
 })
 
-let Upload = mongoose.model('Upload',uploadSchema)
+ let Upload=   mongoose.model('Upload',uploadSchema)
 
-module.exports= Upload
+ module.exports=Upload
