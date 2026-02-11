3b85d5669d71008c08a8d4b54a105a5a6744a399
Anjalirajak205 <anjalirajak205@gmail.com>
2026-02-11 20:55:29 +0530

    11 feb

--- ChatApp/Client/my-react-app/src/main.jsx
+++ ChatApp/Client/my-react-app/src/main.jsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
-// import App from './App.jsx'
+import App from './App.jsx'
 // import SignUp from './SignUp.jsx'
 import LogIn from './Login.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/* <SignUp></SignUp> */}
-    <LogIn></LogIn>
+   <StrictMode>
+    <App />
   </StrictMode>,
 )
